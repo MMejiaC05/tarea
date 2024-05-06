23bda09c159c427d8fcec98dcded836d0f80388f
MMejiaC05 <164411533+MMejiaC05@users.noreply.github.com>
2024-05-06 12:45:21 -0600

    Add files via upload

---
+++ 6-5-24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main {}
+{
+    int  = 1, resultado=0;
+    while (i<13)
+ {
+    resultado=i*12;
+
+    cout << "12 *" <<i<< "=" "resultado << endl"
+    i++;
+ }
+ return 0;
+}
